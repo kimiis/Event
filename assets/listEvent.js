fa2f5289d1e1162a9c1b9ecc649bf20dd3813a98
Kimi <kimi@example.com>
2023-10-18 14:55:44 +0200

    filtre date event moins de 1 mois fait, avec la fonction complete du cancel an event

--- assets/listEvent.js
+++ assets/listEvent.js
@@ -8,10 +8,12 @@ onload = () => {
 function promptCanceled() {
 
     let cancelButton = document.getElementById('cancelButton');
-    cancelButton.addEventListener('click', function () {
-
+    cancelButton.addEventListener('click', function (event) {
+        console.log(event.target.name)
         let reason = prompt("What's the reason?");
         console.log("Reason: " + reason);
-
+        // ici une fois que l'utilisateur à tapé la raison d'annulation, on redirige vers la page d'annulation
+        // on lui passe dans l'url l'id de l'event qui est donc utilisable dans le controller
+        document.location = `/canceled/${event.target.name}/${encodeURI(reason)}`
     });
 }
